7535adad33639a6a0d3bc2cb65cf316fac229b8f
KingGeneral <anthony.gamez09@gmail.com>
2016-08-19 14:33:39 +0700

    mdjsons template added

---
+++ application/classes/Model/mdjsons.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Model_Mdjsons extends Kohana_Model
+{
+	private $table_name = 'mdjsons';
+	
+	public function __construct()
+	{
+		//model
+	}
+
+}
---
+++ application/views/zone/testjson.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+<html>
+    <head>
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- bootstrap -->
+        <?php echo HTML::style('assets/js/bootstrap-3.3.7-dist/css/bootstrap.css') ?>
+        <!-- css -->
+        <?php echo HTML::style('assets/css/style.css');?>
+        <title>CD Collection !!</title>
+        <script type="text/javascript" src="//code.jquery.com/jquery-2.1.0.js"></script>
+    </head>
+    <body>
+        <h1>Testing Json</h1>
+
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            
+        </div>
+
+        <!-- first -->
+        <script type="text/javascript">
+            var x = 0;
+        </script>
+
+        <!--script-->
+        <script type="text/javascript">
+            var based_url = '<?php echo URL::base(TRUE); ?>';
+        </script>
+
+        <?php echo HTML::script('assets/js/jquery/jquery-1.12.4.js') ?>
+        <?php echo HTML::script('assets/js/jquery/jquery-ui.min.js') ?>
+        <?php echo HTML::script('assets/js/bootstrap-3.3.7-dist/js/bootstrap.js') ?>
+        <?php //echo HTML::script('assets/js/jquery.mjs.nestedSortable.js') ?>
+        <?php //echo HTML::script('assets/js/testsend.js') ?>
+        
+    </body>
+</html>
